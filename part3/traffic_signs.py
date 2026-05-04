1f2566bd8016566b4125f971656a7686e5009fc9
Driftedbucket <ndinowix@gmail.com>
2026-05-04 04:22:54 +0200

    Tensor flow import

--- part3/traffic_signs.py
+++ part3/traffic_signs.py
@@ -8,3 +8,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report, confusion_matrix
+
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout, BatchNormalization
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
