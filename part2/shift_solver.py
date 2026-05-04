e934e0ee7b311a18d11ccdf35c353953680ab653
Driftedbucket <ndinowix@gmail.com>
2026-05-04 19:12:27 +0200

    Days and Shift initialization

--- part2/shift_solver.py
+++ part2/shift_solver.py
@@ -5,20 +5,36 @@ from collections import Counter, deque
 from copy import deepcopy
 from pathlib import Path
 
-def __init__(self, staff_file:str|Path):
-def _load_staff(self, staff_file: Path) -> dict[str, set[int]]:
-def _build_neighbors(self) -> None:
-def _parse_variable(self, variable: str) -> tuple[int, str]:
-def _violates_rest_constraint(self, variable_x: str, nurse_x: str, variable_y: str, nurse_y: str)
-def enforce_node_consistency(self) -> None:
-def revise(self, x: str, y: str) -> bool:
-def ac3(self, arcs: list[tuple[str, str]] | None = None) -> bool:
-def assignment_complete(self, assignment: dict[str, str]) -> bool:
-def consistent(self, assignment: dict[str, str]) -> bool:
-def select_unassigned_variable(self, assignment: dict[str, str]) -> str:
-def order_domain_values(self, variable: str, assignment: dict[str, str]) -> list[str]:
-def _forward_check(self, variable: str, nurse: str, assignment: dict[str, str]) -> bool:
-def backtrack(self, assignment: dict[str, str]) -> dict[str, str] | None:
-def solve(self) -> dict[str, str] | None:
-def format_schedule(self, assignment: dict[str, str]) -> str:
-def main() -> None:
+
+class Shift_Solver:
+
+    DAYS = [
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+        "Sunday",
+    ]
+    SHIFTS = ["Morning", "Afternoon", "Night"]
+    MAX_SHIFTS_PER_NURSE = 5
+
+    def __init__(self, staff_file:str|Path):
+
+    def _load_staff(self, staff_file: Path) -> dict[str, set[int]]:
+    def _build_neighbors(self) -> None:
+    def _parse_variable(self, variable: str) -> tuple[int, str]:
+    def _violates_rest_constraint(self, variable_x: str, nurse_x: str, variable_y: str, nurse_y: str)
+    def enforce_node_consistency(self) -> None:
+    def revise(self, x: str, y: str) -> bool:
+    def ac3(self, arcs: list[tuple[str, str]] | None = None) -> bool:
+    def assignment_complete(self, assignment: dict[str, str]) -> bool:
+    def consistent(self, assignment: dict[str, str]) -> bool:
+    def select_unassigned_variable(self, assignment: dict[str, str]) -> str:
+    def order_domain_values(self, variable: str, assignment: dict[str, str]) -> list[str]:
+    def _forward_check(self, variable: str, nurse: str, assignment: dict[str, str]) -> bool:
+    def backtrack(self, assignment: dict[str, str]) -> dict[str, str] | None:
+    def solve(self) -> dict[str, str] | None:
+    def format_schedule(self, assignment: dict[str, str]) -> str:
+    def main() -> None:
