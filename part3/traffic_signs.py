09bb6fc927820a311d7519fa8563a8872c31f539
Driftedbucket <ndinowix@gmail.com>
2026-05-04 04:25:52 +0200

    Method signatures(load_data, build_model, plot_sample, main(entry point))

--- part3/traffic_signs.py
+++ part3/traffic_signs.py
@@ -16,3 +16,18 @@ from tensorflow.keras.utils import to_categorical
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 IMG_SIZE = (30,30)
+
+def load_data(dataset_path, max_per_class=None, sample_fraction=None):
+def build_model(input_shape, num_classes):
+def plot_samples(X, y_true, y_pred, class_map=None, n=8):
+def main():
+
+
+
+
+
+
+
+
+
+
